3c937746f8fa143ae92f7acd77c8014c8077fef5
cyberphysicalrobotics <cyberphysicalrobotics@gmail.com>
2017-02-01 11:23:42 -0500

    added data input file.

--- src/Face.cpp
+++ src/Face.cpp
@@ -88,29 +88,3 @@ int main()
 {
     user();
 }
-
-//#include <iostream>
-//#include <fstream>
-//using namespace std;
-//int main ()
-//{
-//ifstream input_file("afile.dat");
-//if (!input_file.is_open()) {  // check for successful opening
-//cout << "Input file could not be opened! Terminating!" << endl;
-//return 1;   }
-//ofstream output_file("data.output");  // open the output file
-//if (!output_file.is_open()) { // check for successful opening
-//cout << "Output file could not be opened! Terminating!" << endl;
-//return 1;   }
-//// read as long as the stream is good - any problem, just quit.
-//// output is each number times two on a line by itself
-//int datum;
-//while (input_file >> datum) {
-//    cout<<datum;
-//output_file << datum * 2 << endl;
-//}
-//input_file.close();
-//output_file.close();
-//cout << "Done!" << endl;
-//return 0;
-//}
